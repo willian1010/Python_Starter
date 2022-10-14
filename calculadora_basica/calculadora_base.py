ad96b497b132e8e4639fbeb8fe935d7887a89b0b
Willian Oliveira <escapamento2@outlook.com>
2022-10-14 12:42:39 -0300

    Update calculadora_base.py

--- calculadora_basica/calculadora_base.py
+++ calculadora_basica/calculadora_base.py
@@ -5,6 +5,8 @@
 numero1= float (input ('primeiro numero')) #imprime e guarda o valor emformado
 a = input('qual a operação')#imprime e guarda a operacao emformado
 numero2= float (input('segundo numero'))
+
+
 if a == '+': # caso a variavel tiver o mesmo valor da opracao ela é feita
     print (numero1+numero2)#apresenta a soma
 if a == '-':# caso a variavel tiver o mesmo valor da opracao ela é feita
