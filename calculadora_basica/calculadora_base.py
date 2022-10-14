7fed17f8d32fc4b8720d925eba26b244a4458eb4
Willian Oliveira <escapamento2@outlook.com>
2022-10-13 23:26:15 -0300

    calculadora basica com as principais operacoes em python

---
+++ calculadora_basica/calculadora_base.py
@@ -0,0 +1,15 @@
+#Inicio do estudo da linguagem python
+#Programa para calcular numeros inteiros e/ou float operacoes + - / *.
+
+
+numero1= float (input ('primeiro numero')) #imprime e guarda o valor emformado
+a = input('qual a operação')#imprime e guarda a operacao emformado
+numero2= float (input('segundo numero'))
+if a == '+': # caso a variavel tiver o mesmo valor da opracao ela é feita
+    print (numero1+numero2)#apresenta a soma
+if a == '-':# caso a variavel tiver o mesmo valor da opracao ela é feita
+    print (numero1-numero2)#apresenta a subitracao
+if a == '/':# caso a variavel tiver o mesmo valor da opracao ela é feita
+    print (numero1/numero2)#apresenta a divisao
+if a == '*':# caso a variavel tiver o mesmo valor da opracao ela é feita
+    print (numero1*numero2)#apresenta a multiplicacao
