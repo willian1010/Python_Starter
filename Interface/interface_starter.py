541aa5933054e21c5d1d37771f54563558b78bc7
Willian Oliveira <escapamento2@outlook.com>
2022-11-05 19:40:18 -0300

    foi feito uma introducao a interfeces do piton via pysimplegui

---
+++ Interface/interface_starter.py
@@ -0,0 +1,21 @@
+from PySimpleGUI import PySimpleGUI as sg
+
+sg.theme('Reddit')
+layout= [
+    [sg.Text('User'),sg.Input(key='usuario')],
+    [sg.Text('Senha'),sg.Input(key='senha',password_char='*')],
+    [sg.Checkbox('Lembrar de mim?')],
+    [sg.Button('Entrar')],
+    
+]
+
+janela = sg.Window('Tela',layout)
+
+
+while True:
+    enventos, valores =janela.read()
+    if enventos == sg.WINDOW_CLOSED:
+        break
+    if enventos == 'Entrar':
+        if valores['usuario']== 'jao' and valores ['senha']=='000000':
+            print("Bem Vindo")
